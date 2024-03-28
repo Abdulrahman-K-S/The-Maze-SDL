180b7ba3938dcd2083df60c0dc47fa2074bdfca6
Abdurlahman Khaled <21-101079@students.eui.edu.eg>
2024-03-29 01:34:51 +0200

    Update main.c
    Added the well built main

--- src/main.c
+++ src/main.c
@@ -1,90 +1,176 @@
-#include "maze.h"
-#include "map.h"
-#include "config.h"
-#include "player.h"
+#include <stdio.h>
+#include <stdlib.h>
 
-const short maze[MAP_GRID_HEIGHT][MAP_GRID_WIDTH] = {
-	{R,R,R,R,R,R,R,R},
-	{R,0,0,0,0,0,0,R},
-	{R,0,0,0,0,0,0,R},
-	{R,0,0,0,0,0,0,R},
-	{R,0,0,0,0,P,0,R},
-	{R,0,0,0,0,0,0,R},
-	{R,0,0,0,0,0,0,R},
-	{R,R,R,R,R,R,R,R},
+#include "config.h"
+#include "raycaster.h"
+#include "renderer.h"
+#include "player.h"
+#include "map.h"
+
+const short MAP[MAP_GRID_HEIGHT][MAP_GRID_WIDTH] = {
+    {B,B,B,B,B,B,B,B,B,B},
+    {B,0,0,0,0,0,0,0,0,B},
+    {B,0,0,R,R,0,0,0,0,B},
+    {B,0,0,R,G,0,0,0,0,B},
+    {B,0,0,0,0,0,0,0,0,B},
+    {B,0,0,0,0,G,R,0,0,B},
+    {B,0,0,0,0,R,R,0,0,B},
+    {B,0,0,0,0,0,0,0,0,B},
+    {B,0,0,0,0,0,0,0,0,B},
+    {B,B,B,B,B,B,B,B,B,B}
 };
 
-int gameIsRunning = TRUE;
-int showMap = FALSE;
 
-int main(int argc, char* args[])
+/* Program globals */
+
+const Uint32 COLORS[4] = {
+    RGBtoABGR(255, 0, 0),
+    RGBtoABGR(0, 255, 0),
+    RGBtoABGR(0, 0, 255),
+    RGBtoABGR(128, 128, 128)
+};
+
+/* Program toggles */
+char gameIsRunning = TRUE;
+char showMap = FALSE;
+
+void render()
 {
-	(void)argc;
-	(void)args;
-	SDL_Instance instance;
-
-	init_instance(&instance);
-	initPlayer();
-
-	while (gameIsRunning)
-	{
-		pollEvent();
-
-		COLOR_BLACK(instance.renderer);
-		SDL_RenderClear(instance.renderer);
-
-		if (showMap)
-		{
-			drawMap(&instance);
-			drawPlayer(&instance);
-		}
-		updatePlayer();
-		//castRays(&instance);
-
-		SDL_RenderPresent(instance.renderer);
-	}
-
-	SDL_DestroyRenderer(instance.renderer);
-	SDL_DestroyWindow(instance.window);
-	SDL_Quit();
-	return 0;
+    if (showMap)
+    {
+        clearRenderer();
+        renderMap();
+    }
+    else
+    {
+        /* Draw projected scene */
+        renderProjectedScene();
+    }
 }
 
-int pollEvent()
+void consumeSDLEvents()
 {
-	SDL_Event event;
-	char keyIsDown;
-	while (SDL_PollEvent(&event))
-	{
-		keyIsDown = (event.type == SDL_KEYDOWN);
-		switch (event.type)
-		{
-		case SDL_QUIT:
-			gameIsRunning = FALSE;
-			break;
-		case SDL_KEYDOWN:
-			switch (event.key.keysym.sym)
-			{
-			case SDLK_w:
-				movingForward = keyIsDown;
-				break;
-			case SDLK_s:
-				movingBack = keyIsDown;
-				break;
-			case SDLK_d:
-				turningRight = keyIsDown;
-				break;
-			case SDLK_a:
-				turningLeft = keyIsDown;
-				break;
-			case SDLK_m:
-				if (keyIsDown) showMap = !showMap;
-			case SDLK_LSHIFT:
-				playerIsRunning = keyIsDown;
-				break;
-			}
-			break;
-		}
-	}
-	return (0);
+    SDL_Event event;
+    char keyIsDown;
+
+    while (SDL_PollEvent(&event))
+    {
+        keyIsDown = (event.type == SDL_KEYDOWN);
+        switch (event.type)
+        {
+        case SDL_KEYUP:
+        case SDL_KEYDOWN:
+            switch (event.key.keysym.sym)
+            {
+            case SDLK_UP:
+            case SDLK_w:
+                movingForward = keyIsDown;
+                break;
+            case SDLK_DOWN:
+            case SDLK_s:
+                movingBack = keyIsDown;
+                break;
+            case SDLK_LEFT:
+            case SDLK_a:
+                turningLeft = keyIsDown;
+                break;
+            case SDLK_RIGHT:
+            case SDLK_d:
+                turningRight = keyIsDown;
+                break;
+            case SDLK_LSHIFT:
+            case SDLK_RSHIFT:
+                playerIsRunning = keyIsDown;
+                break;
+            case SDLK_ESCAPE:
+                gameIsRunning = FALSE;
+                break;
+            case SDLK_m:
+                if (keyIsDown) showMap = !showMap;
+                break;
+            case SDLK_LEFTBRACKET:
+                if (keyIsDown && distFromViewplane - 20.0f > 100.0f) distFromViewplane -= 20.0f;
+                break;
+            case SDLK_RIGHTBRACKET:
+                if (keyIsDown) distFromViewplane += 20.0f;
+                break;
+            default:
+                break;
+            }
+            break;
+        case SDL_QUIT:
+            gameIsRunning = FALSE;
+            break;
+        default:
+            break;
+        }
+    }
+}
+
+void runGame()
+{
+    long gameTicks = 0;
+    long time;
+
+    do
+    {
+        time = SDL_GetTicks();
+
+        /* Handle SDL key events */
+        consumeSDLEvents();
+
+        /* Update the player */
+        updatePlayer();
+
+        /* Update the raycaster */
+        updateRaycaster();
+
+        /* Render a frame */
+        render();
+
+        /* Fixed delay before next frame */
+        SDL_Delay(10);
+
+        /* Print FPS every 500 frames */
+        if (!(gameTicks++ % 500))
+            fprintf(stderr, "FPS: %.2f\n", 1000.0f / (float)(SDL_GetTicks() - time));
+    } while (gameIsRunning);
+}
+
+int setupWindow()
+{
+    int x, y;
+
+    if (!initGFX("Raycaster", WINDOW_WIDTH, WINDOW_HEIGHT)) return FALSE;
+
+    screenBuffer = createTexture(WINDOW_WIDTH, WINDOW_HEIGHT);
+
+    if (!screenBuffer) return FALSE;
+
+    /* Make the texture initially gray */
+    for (x = 0; x < WINDOW_WIDTH; x++)
+    {
+        for (y = 0; y < WINDOW_HEIGHT; y++)
+        {
+            screenBuffer[(WINDOW_WIDTH * y) + x] = 0xFFAAAAAA;
+        }
+    }
+        
+
+    return TRUE;
+}
+
+int main()
+{
+    if (!setupWindow())
+    {
+        fprintf(stderr, "Could not initialize raycaster!\n");
+        return EXIT_FAILURE;
+    }
+    initPlayer();
+    initRaycaster();
+    runGame();
+
+    destroyGFX();
+    return EXIT_SUCCESS;
 }
